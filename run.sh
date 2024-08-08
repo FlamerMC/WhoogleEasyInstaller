9e3524d9cedd11f1ad67953eb9c8ae8e9928079e
FlamerMC <153970526+FlamerMC@users.noreply.github.com>
2024-08-08 22:40:14 +0530

    idk

---
+++ run.sh
@@ -0,0 +1,16 @@
+echo going into home/$USER
+wait 1
+clear
+cd ~
+echo Starting venv
+wait 1
+clear
+source Whoogle/bin/activate
+echo Starting Whoogle
+wait 1
+clear
+cd Whoogle
+whoogle-search
+wait 1
+clear
+echo Whoogle Started Logs Bellow 
---
+++ whoogle-install.sh
@@ -0,0 +1,43 @@
+
+clear
+sleep 1
+echo going into home/$USER
+
+echo cd ~
+cd ~
+sleep 1
+clear
+echo Making the Whoogle folder
+mkdir Whoogle
+echo mkdir Whoogle
+sleep 1
+clear
+echo Making venv envir
+python3 -m venv Whoogle
+echo python3 -m venv Whoogle
+sleep 1
+clear
+echo Starting venv
+source floogle/bin/activate
+echo source floogle/bin/activate
+sleep 1
+clear
+echo Going into "Whoogle" Directory
+cd Whoogle
+echo Whoogle
+sleep 1
+clear
+echo Installing whoogle-search
+pip install whoogle-search
+echo pip install whoogle-search
+sleep 1
+clear
+echo Getting run.sh from github
+echo NOT COMPLETE
+sleep 1
+clear
+echo To start whoogle just run home/$USER/Whoogle/run.sh
+sleep 1
+clear
+echo Starting whoogle server at http://127.0.0.1:5000/
+whoogle-search 
